2955ebff2c258be037b7e039d2ea303d0879467c
Raiyan007-gb <raiyan2025@gmail.com>
2026-07-13 23:12:07 +0600

    chore : repo name changed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "thirdspace.toronto.edu";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? `/${repoName}` : "",
+  basePath: "",
   images: {
     unoptimized: true,
   },

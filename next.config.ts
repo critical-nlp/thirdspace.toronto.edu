afd577b186c3196f2562f5e25d3245e4ff256b92
Raiyan007-gb <raiyan2025@gmail.com>
2026-07-13 23:33:48 +0600

    chore : revert

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "thirdspace.toronto.edu";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "",
+  basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true,
   },
